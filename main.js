1b269f2414a9a9a8769a2c1088b9b3c2a66f4bcf
Luis <lgfvarella@gmail.com>
2023-07-21 16:33:34 -0300

    add layout 1024px 50%

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const swiper = new Swiper('.swiper', {
           spaceBetween: 40,
         },
         1024: {
-          slidesPerView: 5,
+          slidesPerView: 3,
           spaceBetween: 50,
         },
       },
